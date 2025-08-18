1845f4c5eddc81fb0796fa9e54c3899fe69f85e0
ShreyMadaan <89932358+ShreyMadaan@users.noreply.github.com>
2025-08-18 20:43:22 +0530

    feat: add exit option to BankApp menu

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Bank Application");
         System.out.println("Do you want to create an account?");
+        System.out.println("0. Exit");
         System.out.println("1. Yes");
         System.out.println("2. No");
+        System.out.println("Please Enter your choice");
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
         if(choice == 1){
@@ -17,6 +19,12 @@ public class Main {
             double balance = sc.nextDouble();
             BankAccount account = new BankAccount(accountNumber, name, balance);
             System.out.println("Account created successfully");
+        }else if(choice == 2){
+            System.out.println("Currently we don't provide any other features. Stay tuned for future updates");
+        }else if(choice == 0){
+            System.out.println("Thank you for Banking with us");
+        }else{
+            System.out.println("Invalid choice");
         }
     }
 }
