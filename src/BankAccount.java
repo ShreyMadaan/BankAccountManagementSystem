1bc3a5e705abe37beb815bbf34907ee31eae2537
ShreyMadaan <89932358+ShreyMadaan@users.noreply.github.com>
2025-08-18 20:28:26 +0530

    feat: add BankAccount class with basic attributes

---
+++ src/BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(int accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+}
