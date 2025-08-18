f7a4d5b3500a650cb57ec6c4c5dacc4a0946e647
ShreyMadaan <89932358+ShreyMadaan@users.noreply.github.com>
2025-08-18 20:30:57 +0530

    feat: add deposit and withdraw methods to BankAccount

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -8,4 +8,18 @@ public class BankAccount {
         this.accountHolderName = accountHolderName;
         this.balance = balance;
     }
+
+    public double deposit(double amount){
+        balance += amount;
+        return balance;
+    }
+
+    public double withdraw(double amount){
+        if(amount > balance){
+            System.out.println("Insufficient Funds");
+            return balance;
+        }
+        balance -= amount;
+        return balance;
+    }
 }
